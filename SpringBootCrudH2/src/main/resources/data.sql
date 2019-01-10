2794acec9ca988ace489f44af6096fecc0805954
Wilfred Bett <kimutaibett30@gmail.com>
2019-01-10 11:20:24 +0300

    Update data.sql

--- SpringBootCrudH2/src/main/resources/data.sql
+++ SpringBootCrudH2/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO Student
-values(101,'Math','Grade8','Shariq');
+values(101,'Math','Grade8','Wilfred Bett');
 
 INSERT INTO Student
-values(102,'Hindi','Garde7','Manish');
+values(102,'English','Garde7','Bett');
 
 INSERT INTO Student
-values(103,'Computer','Grade9','Chandra Mani');
+values(103,'Computer','Grade9','Kimutai');
